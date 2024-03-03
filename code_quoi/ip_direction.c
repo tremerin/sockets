e1804ff6f9364a1eb1e3fe9876e2901e264f7441
fgalan-r <tremerin13@gmail.com>
2024-03-03 05:48:27 +0100

    ping 2.0

--- code_quoi/ip_direction.c
+++ code_quoi/ip_direction.c
@@ -3,21 +3,22 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int ac, char **av) {
-	struct addrinfo hints; // Indications pour getaddrinfo()
-	struct addrinfo *res;  // Résultat de getaddrinfo()
-	struct addrinfo *r;    // Pointeur pour itérer sur les résultats
-	int status; // Valeur de retour de getaddrinfo()
-	char buffer[INET6_ADDRSTRLEN]; // Buffer pour reconvertir l'adresse IP
+int main(int ac, char **av)
+{
+	struct addrinfo hints;           // Indications pour getaddrinfo()
+	struct addrinfo *res;            // Résultat de getaddrinfo()
+	struct addrinfo *r;              // Pointeur pour itérer sur les résultats
+	int status;                      // Valeur de retour de getaddrinfo()
+	char buffer[INET6_ADDRSTRLEN];   // Buffer pour reconvertir l'adresse IP
 
 	if (ac != 2)
 	{
 		fprintf(stderr, "usage: /a.out hostname\n");
 		return (1);
 	}
-	memset(&hints, 0, sizeof hints); // Initialise la structure
-	hints.ai_family = AF_UNSPEC; // IPv4 ou IPv6
-	hints.ai_socktype = SOCK_STREAM; // TCP
+	memset(&hints, 0, sizeof hints);  // Initialise la structure
+	hints.ai_family = AF_UNSPEC;      // IPv4 ou IPv6
+	hints.ai_socktype = SOCK_STREAM;  // TCP
 
 	// Récupère la ou les adresses IP associées
 	status = getaddrinfo(av[1], 0, &hints, &res);
@@ -32,13 +33,11 @@ int main(int ac, char **av) {
 	r = res;
 	while (r != NULL)
 	{
-		void *addr; // Pointeur vers l'adresse IP
-		if (r->ai_family == AF_INET) { // Adresse IPv4
-			// il faut caster l'adresse en structure sockaddr_in pour récupérer
-			// l'adresse IP, comme le champ ai_addr pourrait être soit
-			// un sockaddr_in soit un sockaddr_in6
+		void *addr;
+		// IPv4
+		if (r->ai_family == AF_INET)
+		{ 
 			struct sockaddr_in *ipv4 = (struct sockaddr_in *)r->ai_addr;
-			// Transforme l'entier en adresse IP lisible
 			inet_ntop(r->ai_family, &(ipv4->sin_addr), buffer, sizeof buffer);
 			printf("IPv4: %s\n", buffer);
 		}
@@ -48,8 +47,8 @@ int main(int ac, char **av) {
 			inet_ntop(r->ai_family, &(ipv6->sin6_addr), buffer, sizeof buffer);
 			printf("IPv6: %s\n", buffer);
 		}
-		r = r->ai_next; // Prochaine adresse renseignée par getaddrinfo()
+		r = r->ai_next;
 	}
-	freeaddrinfo(res); // Libère la mémoire
+	freeaddrinfo(res);
 	return (0);
 }
